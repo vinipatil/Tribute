0b2f6b86872132765550fdca3bfd2a17b96b0b41
Vinita Patil <vinipatil2325@gmail.com>
2024-04-11 18:33:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Tribute from './Tribute';
+
+function App() {
+  return (
+    <div className="App">
+      <Tribute />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tribute.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChild } from '@fortawesome/free-solid-svg-icons';
+import { faSchool } from '@fortawesome/free-solid-svg-icons';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { faFlag } from '@fortawesome/free-solid-svg-icons';
+import { faHandsHelping } from '@fortawesome/free-solid-svg-icons';
+import { faBalanceScale } from '@fortawesome/free-solid-svg-icons';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import './App.css'
+
+function Tribute() {
+    return (
+        <div className="tribute-container">
+            <h1>Tribute to Mahatma Gandhi <br></br> Father of our Nation</h1>
+            <div className="tribute-cont">
+                <img src="https://i.ytimg.com/vi/1CcW_ySDP0g/maxresdefault.jpg" alt="Mahatma Gandhi" />
+                <h1>Mahatma Gandhi</h1>
+            </div>
+            <div className='info'>
+                <div className='para'>
+                    <p className='p0'>Childhood <FontAwesomeIcon icon={faChild} /></p><br></br>
+                    <p className='p1'>Mohandas Karamchand Gandhi, commonly known as Mahatma Gandhi, was born on October 2, 1869, in Porbandar, in the present-day Indian state of Gujarat.was raised in a devout Hindu family. His father was the chief minister of Porbandar, and his mother was deeply religious. Gandhi was a shy and studious child who showed little interest in sports but had a strong moral compass from an early age. At the age of 13, he married Kasturba in an arranged marriage, as was customary in India at the time. Gandhi's family values, religious upbringing, and exposure to the principles of truth and nonviolence from Hindu scriptures laid the foundation for his later philosophy and activism. He was small and dark, and looked no different from the millions of other children born in India. Yet this was no ordinary child.</p>
+                </div>
+                <div className='para-img'>
+                    <img src='https://www.ecoleglobale.com/blog/wp-content/uploads/2020/10/b654dcef-473f-419b-afce-3c3ac19bdc6e.jpg'></img>
+                </div>
+
+                <div className='para-img'>
+                    <img src='https://images.hindustantimes.com/rf/image_size_800x600/HT/p1/2011/08/10/Incoming/Pictures/731765_Wallpaper2.jpg'></img>
+                </div>
+                <div className='para'>
+                    <p className='p0'>Early Life <FontAwesomeIcon icon={faSchool} /></p><br></br>
+                    <p className='p2'>In his early years, Gandhi was deeply influenced by the stories of Shravana and Harishchandra that reflected the importance of truth. Through these stories and from his personal experiences, he realized that truth and love are among the supreme values. Mohandas married Kasturba Makhanji at the age of 13. Gandhi later went on to reveal that the marriage didn’t mean anything to him at that age and that he was happy and excited only about wearing new set of clothes.At the age of 19, Mohandas left home to study law in London at the Inner Temple, one of the city’s four law colleges. Upon returning to India in mid-1891, he set up a law practice in Bombay, but met with little success. He soon accepted a position with an Indian firm that sent him to its office in South Africa. Along with his wife, Kasturbai, and their children, Gandhi remained in South Africa for nearly 20 years.</p>
+                </div>
+
+                <div className='para'>
+                    <p className='p0'>Education <FontAwesomeIcon icon={faGraduationCap} /></p><br></br>
+                    <p className='p1'>After his family moved to Rajkot, a nine year old Gandhi was enrolled at a local school, where he studied the basics of arithmetic, history, geography and languages. When he was 11 years old, he attended a high school in Rajkot. He lost an academic year in between because of his wedding but later rejoined the school and eventually completed his schooling. He then dropped out of Samaldas College in Bhavnagar State after joining it in the year 1888. Later Gandhi was advised by a family friend Mavji Dave Joshiji to pursue law in London.Supported by his brother, Gandhi left to London and attended the Inner Temple and practiced law. During his stay in London, Gandhi joined a Vegetarian Society and was soon introduced to Bhagavad Gita by some of his vegetarian friends. The contents of Bhagavad Gita would later have a massive influence on his life. </p>
+                </div>
+                <div className='para-img'>
+                    <img src='https://cdn.leverageedu.com/blog/wp-content/uploads/2020/05/16211601/Mahatma-Gandhi-Educational-Quote.png'></img>
+                </div>
+
+                <div className='para-img'>
+                    <img src='https://st.adda247.com/https://s3-ap-south-1.amazonaws.com/adda247jobs-wp-assets-adda247/articles/wp-content/uploads/2023/03/01114915/Gandhi-in-South-Africa.jpg'></img>
+                </div>
+                <div className='para'>
+                    <p className='p0'>Gandhi in South Africa <FontAwesomeIcon icon={faFlag} /></p><br></br>
+                    <p className='p2'>After returning to India, Gandhi struggled to find work as a lawyer. In 1893, Dada Abdullah, a merchant who owned a shipping business in South Africa asked if he would be interested to serve as his cousin’s lawyer in South Africa. Gandhi gladly accepted the offer and left to South Africa, which would serve as a turning point in his political career.In South Africa, he faced racial discrimination directed towards blacks and Indians. He faced humiliation on many occasions but made up his mind to fight for his rights. This turned him into an activist and he took upon him many cases that would benefit the Indians and other minorities living in South Africa.After spending 21 years in South Africa, where he fought for civil rights, he had transformed into a new person and he returned to India in 1915. </p>
+                </div>
+            </div><br></br><br></br>
+
+            <h2 className='p0'><FontAwesomeIcon icon={faHandsHelping} /> Achievements <FontAwesomeIcon icon={faHandsHelping} /></h2>
+            <div className='info'>
+                <div className='para'>
+                    <p className='p0'>The Development of Satyagraha</p><br></br>
+                    <p className='p1'>The term "Satyagraha" was first coined by Gandhi in South Africa in 1906 during the struggle against racial discrimination faced by Indians. It combines two Sanskrit words: "Satya" meaning truth and "agraha" meaning insistence or holding firmly to. Thus, Satyagraha can be understood as "truth force" or "soul force."Satyagraha is built on the principle of ahimsa, or nonviolence, which Gandhi believed to be the highest form of moral conduct.Satyagraha emphasizes the importance of truth, courage, and self-suffering in the pursuit of justice. It encourages individuals to resist injustice without resorting to violence, even in the face of repression.Upon returning to India in 1915, Gandhi brought Satyagraha with him and applied it to the Indian freedom struggle. He led several campaigns, including the Non-Cooperation Movement, Civil Disobedience Movement, all based on the principles of Satyagraha.</p>
+                </div>
+                <div className='para-img'>
+                    <img src='https://i0.wp.com/andedge.com/wp-content/uploads/2021/02/Satyagraha-movement-of-Gandhiji-and-Growth-of-Satyagraha-Upsc.jpg?resize=768%2C516&ssl=1'></img>
+                </div>
+
+                <div className='para-img'>
+                    <img src='https://c8.alamy.com/comp/E0KPBD/mahatma-gandhi-surrounded-by-his-friends-on-his-arrival-in-delhi-E0KPBD.jpg'></img>
+                </div>
+                <div className='para'>
+                    <p className='p0'>Achieving India’s Independence</p><br></br>
+                    <p className='p1'>Mahatma Gandhi played a pivotal role in India's struggle for independence from British colonial rule. His approach was characterized by nonviolent resistance, civil disobedience.</p><br></br>
+                    <p className='p3'>
+                        <ul>
+                            <li><strong>Leadership in the Indian National Congress:</strong> Gandhi became involved in the Indian National Congress (INC), the leading political party advocating for Indian independence. He emerged as a prominent leader within the INC and used his platform to promote his philosophy of nonviolence and civil disobedience.</li>
+                            <li><strong>Salt March (Dandi March):</strong> One of Gandhi's most famous acts of civil disobedience was the Salt March in 1930. In response to the British monopoly on salt production and the salt tax, Gandhi led a 240-mile march to the coastal town of Dandi, where he and his followers made salt from seawater in defiance of British law.</li>
+                            <li><strong>Quit India Movement:</strong> In 1942, Gandhi launched the Quit India Movement, demanding an end to British rule in India.Although the movement faced harsh repression from the British authorities, it further intensified the demand for independence.</li>
+                            <li><strong>Legacy:</strong> India finally gained independence from British rule on August 15, 1947. Mahatma Gandhi's leadership, philosophy of nonviolence, and commitment to social justice played a significant role in achieving this historic milestone. He remains revered as the "Father of the Nation" in India and continues to inspire movements for peace, justice, and human rights worldwide.</li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+
+            <div className='philo'>
+                <h1><FontAwesomeIcon icon={faBalanceScale} /> Mahatma Gandhi's philosophy <FontAwesomeIcon icon={faBalanceScale} /></h1><br></br>
+                <p>Mahatma Gandhi's philosophy was deeply rooted in the principles of truth, nonviolence (ahimsa), and the pursuit of justice. He developed his philosophy through personal experiences, study of religious texts, and engagement with social and political issues.</p><br></br>
+                <p>
+                    <ol>
+                        <li><strong>Ahimsa (Nonviolence):</strong> Central to Gandhi's philosophy was the principle of ahimsa, or nonviolence. He believed that violence only begets more violence and that true change could only be achieved through nonviolent means. Ahimsa extended beyond physical violence to include noncooperation with unjust laws, economic exploitation, and social discrimination.</li>
+                        <li><strong>Sarvodaya (Welfare of All):</strong> Gandhi advocated for the principle of "Sarvodaya," which means the welfare of all. He believed in creating a society based on equality, justice, and compassion, where the needs of the poorest and most vulnerable are prioritized.</li>
+                        <li><strong>Satyagraha (Truth Force):</strong> Gandhi coined the term "Satyagraha" to describe his philosophy of nonviolent resistance. Satyagraha is based on the idea that truth (satya) is the ultimate force in the universe and that individuals can resist oppression and injustice through the power of truth and nonviolence.</li>
+                        <li><strong>Swadeshi (Self-Sufficiency):</strong> Gandhi promoted the idea of "Swadeshi," or self-sufficiency, as a means of economic empowerment and resistance to colonialism. He encouraged Indians to produce their own goods, particularly through the revival of cottage industries like spinning and weaving.</li>
+                        <li><strong>Simple Living:</strong> Gandhi lived a simple and austere life, with a focus on moral and spiritual values rather than material possessions. He believed in leading by example and practiced what he preached, wearing simple clothes, eating a vegetarian diet, and living in communal ashrams.</li>
+                        <li><strong>Religious Pluralism:</strong> Although deeply rooted in Hinduism, Gandhi embraced religious pluralism and believed in the unity of all religions. He drew inspiration from various religious traditions, including Christianity, Islam, Buddhism, and Jainism, and saw religion as a means of promoting love, compassion, and social harmony.</li>
+                    </ol>
+                </p>
+
+                <div className='quotes'>
+                <h1><FontAwesomeIcon icon={faQuoteLeft} /> Mahatma Gandhi's Quotes <FontAwesomeIcon icon={faQuoteRight} /></h1>
+                <ul>
+                    <li><strong>"Be the change that you wish to see in the world."</strong> This quote emphasizes the importance of personal responsibility and proactive action in creating positive change.</li>
+                    <li><strong>"The best way to find yourself is to lose yourself in the service of others."</strong> Gandhi believed in the transformative power of selfless service and altruism.</li>
+                    <li><strong>"An eye for an eye only ends up making the whole world blind." </strong>This quote underscores Gandhi's unwavering commitment to nonviolence and his belief in the futility of revenge and retaliation.</li>
+                    <li><strong>"The weak can never forgive. Forgiveness is the attribute of the strong."</strong> Gandhi advocated for forgiveness and reconciliation as essential components of conflict resolution and peacebuilding.</li>
+                    <li><strong>"First they ignore you, then they laugh at you, then they fight you, then you win." </strong>This quote reflects Gandhi's belief in the power of perseverance and nonviolent resistance in the face of opposition and adversity.</li>
+                    <li><strong>"In a gentle way, you can shake the world."</strong> Gandhi believed in the power of small, incremental actions to effect meaningful change on a global scale.</li>
+                    <li><strong>"Happiness is when what you think, what you say, and what you do are in harmony." </strong>This quote emphasizes the importance of living a life of integrity and alignment with one's values.</li>
+                    <li><strong>"The future depends on what you do today." </strong>Gandhi urged individuals to take action in the present moment to shape a better future for themselves and others.</li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Tribute;
